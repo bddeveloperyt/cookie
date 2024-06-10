2aa20723bbc45135b689b7df0e71caf7f4b40cc0
BDDEVELOPER <81234207+bddeveloperyt@users.noreply.github.com>
2024-06-10 22:27:25 +0600

    Update cookie.php

--- cookie.php
+++ cookie.php
@@ -1,23 +1,17 @@
 <?php
 
+
+
 $response2 = file_get_contents('https://raw.githubusercontent.com/byte-capsule/Toffee-Channels-Link-Headers/main/toffee_channel_data.json');
 
-if ($response2 === FALSE) {
-    die('Error occurred while fetching JSON data.');
-}
-
 $json_data2 = json_decode($response2, true);
+$link2=$json_data2["channels"]["0"]["headers"]["cookie"];
 
-if ($json_data2 === NULL) {
-    die('Error occurred while decoding JSON data.');
-}
 
-$link2 = $json_data2["channels"][0]["headers"]["cookie"];
 
-if (empty($link2)) {
-    die('Error: Cookie value not found.');
-}
+$cookie2 = $link2;
+header("location:$cookie2");
+echo $cookie2;
 
-echo $link2;
 
 ?>
